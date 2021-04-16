3aebc5f8ec17e7e974b46b4bee0ca4dbc96cdff2
Moris Doratiotto <moris.doratiotto@gmail.com>
2021-04-16 22:27:11 +0200

    Encrypt goes bbrrr

--- trickshot.py
+++ trickshot.py
@@ -1,5 +1,6 @@
 from json import loads
 from os import path, walk
+from re import sub
 from time import sleep
 
 from colorifix.colorifix import Color, paint
@@ -8,8 +9,72 @@ from requests import post
 from youtube_dl import YoutubeDL
 
 API_URL = "https://myfreemp3music.com/api/search.php"
-SONGS_FOLDER = "/Users/Moris/Desktop/songs"
-DOWNLOADED_SONG_FOLDER = "MyFreeMP3"
+SONGS_FOLDER = ""
+DOWNLOADED_SONG_FOLDER = ""
+
+
+def encryptD(number):
+    o = [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "J",
+        "K",
+        "M",
+        "N",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "j",
+        "k",
+        "m",
+        "n",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "x",
+        "y",
+        "z",
+        "1",
+        "2",
+        "3",
+    ]
+    length = len(o)
+    e = ""
+    if number < 0:
+        number *= -1
+        e = "-"
+    if number == 0:
+        return o[0]
+    while number > 0:
+        e += o[number % length]
+        number //= length
+    return e
 
 
 def get_songs_list(query):
@@ -22,7 +87,11 @@ def get_download_link_first_song(songs_list):
     response = songs_list.get("response")
     if not response:
         return None, None
-    return response[1].get("title"), response[1].get("url")
+    song = response[1]
+    aid = song.get("id")
+    oid = song.get("owner_id")
+    url = f"https://speed.idmp3s.com/{encryptD(oid)}:{encryptD(aid)}"
+    return song.get("title"), url
 
 
 def download_audio(url, filename):
@@ -38,6 +107,7 @@ def main():
     _, _, files = list(walk(SONGS_FOLDER))[0]
     for file in files:
         query = path.splitext(file)[0].replace("_", " ")
+        query = sub(r"\(.*\)", "", sub(r"\[.*\]", "", query))
         # MyFreeMP3
         SPINNER.start(paint("Searching ", Color.WHITE) + paint(query, Color.BLUE))
         for _ in range(5):
